006eff47637c99830154175cd904caca95d98c1f
Mohit <94623283+ErElectrical@users.noreply.github.com>
2022-01-28 12:02:43 +0000

    Added : DataModel class AddressBook

---
+++ AddressBookDataModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookDB
+{
+    public class AddressBookDataModel
+    {
+        public int id { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+
+        public string phonenum { get; set; }
+
+        public string state { get; set; }
+
+        public string address { get; set; }
+
+     
+
+        public string country { get; set; }
+
+        public string zipcode { get; set; }
+
+        public string emailid { get; set; }
+
+
+
+
+
+
+
+
+
+    }
+}
