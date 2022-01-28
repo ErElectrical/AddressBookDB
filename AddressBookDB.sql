bf509566c6a30efa62e563797ee53ef43093cfd9
Mohit <94623283+ErElectrical@users.noreply.github.com>
2022-01-28 04:05:31 +0000

    Added : Insert data to table

--- AddressBookDB.sql
+++ AddressBookDB.sql
@@ -21,3 +21,10 @@ create table AddressBookTable
 );
 
 select * from AddressBookTable;
+
+--uc3 ability to insert data into table
+
+INSERT  AddressBookTable VALUES('Mohit ','Tanwar','Kotul Akole',422610,'Maharashtra','india',8792810271,'Mohit@gmail.com'),
+('Neha','Shelke','Warora Nagpur',3333333,'Chandigad','india',8232322323,'neha@gmail.com'),
+('Srujan','Mahajan','Dombivali Mumbai',444444,'Maharashtra','japan',9112121212,'srujan@gmail.com'),
+('Prajwal','Zurunge','Chandannagar Vimannagar',555555,'gujrat','india',9876543322,'prajwal@gmail.com');
